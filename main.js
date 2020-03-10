1f38f0ab85bce6449335798a6b5d83af56853533
MdAbrar585 <abrarm585@gmail.com>
2020-03-10 23:42:22 +0600

    first commit

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
 
 function submitIssue(e) {
+  const i = document.getElementById("issueNo").innerText;
+  const iNumber = parseFloat(i);
+  document.getElementById("issueNo").innerText = iNumber + 1;
   const getInputValue = id => document.getElementById(id).value;
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
+  const id = Math.floor(Math.random() * 100000000) + '';
   const status = 'Open';
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
-  if (localStorage.getItem('issues')){
+  if (localStorage.getItem('issues')) {
     issues = JSON.parse(localStorage.getItem('issues'));
   }
   issues.push(issue);
@@ -21,18 +24,33 @@ function submitIssue(e) {
   e.preventDefault();
 }
 
-const closeIssue = id => {
+function setStatusClosed(id) {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => issue.id == id);
   currentIssue.status = 'Closed';
+  currentIssue.description = document.getElementById("description").style.display = `<del>` + currentIssue.description + `</del>`;
+  const j = document.getElementById("closedIssue").innerText;
+  const jNumber = parseFloat(j);
+  document.getElementById("closedIssue").innerText = jNumber + 1;
+  const i = document.getElementById("issueNo").innerText;
+  const iNumber = parseFloat(i);
+  document.getElementById("issueNo").innerText = iNumber - 1;
+
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
-
-const deleteIssue = id => {
+function deleteIssue(id) {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
-  localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  const remainingIssues = issues.filter(issue => issue.id == id);
+  // localStorage.removeItem = remainingIssues;
+  //  console.log(issues.filter(issue => issue.id == id));
+  issues.splice(remainingIssues, 1);
+  const i = document.getElementById("issueNo").innerText;
+  const iNumber = parseFloat(i);
+  document.getElementById("issueNo").innerText = iNumber - 1;
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
 }
 
 const fetchIssues = () => {
@@ -41,12 +59,12 @@ const fetchIssues = () => {
   issuesList.innerHTML = '';
 
   for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
+    const { id, description, severity, assignedTo, status } = issues[i];
 
-    issuesList.innerHTML +=   `<div class="well">
+    issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3 id="description"> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
                               <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
